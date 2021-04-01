bf9e6652dfd799467fe4b621e8caaa461dbbcabe
gusta <gusta@LAPTOP-00APUO4I>
2021-04-01 16:17:48 -0300

    Conexao DB

---
+++ PI3Konex_Rapina/src/main/java/ConexaoBD/ConxaoJDBC.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConexaoBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author gusta
+ */
+public class ConxaoJDBC {
+    public Connection obterConexaoBD() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException("Driver do MySQL não encontrado", ex);
+        }
+        Connection conexao = DriverManager.getConnection("jdbc:mysql://local"
+                + "host:3306/exemplojdbc?useUnicode=yes&"
+                + "characterEncoding=UTF-8&useTimezone=America/Sao_Paulo", "root", "");
+        return conexao;
+
+    }
+}
