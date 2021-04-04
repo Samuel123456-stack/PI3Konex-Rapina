bc26f26631da6384ba106a7430d1d60f95b77a10
Gabriel <Gabriel@JosselinoDesktop>
2021-04-04 13:34:56 -0300

    Implementação dos astribuos e preparação dos metodos que serão utilizados

--- PI3Konex_Rapina/src/main/java/Classes/Estabelecimento.java
+++ PI3Konex_Rapina/src/main/java/Classes/Estabelecimento.java
@@ -1,14 +1,216 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Classes;
 
 /**
  *
- * @author zica_
+ * @author Gabriel Felipe
  */
 public class Estabelecimento {
+    //Atributos do Estabelecimento
+    public int id_estabelecimento;
+    public String nome_estabelecimento;
+    private int cpnj;
+    public int cep_estabelecimento;
+    public String logadouro_estabelecimento;
+    public int numero;
+    public String acessi_auditiva ;
+    public String acessi_fisico ;
+    public String acessi_nenhuma ;
+    public String horario_funcionamento;
+    public int capacidade;
+    public float taxa_cancelamento;
+    private int celular;
+    private String email;
+    private String senha;
+    private String concorda;
+    public String concorda_newstalleter;
+    public boolean disponibilidade;
+    private int id_planos;
+    private int id_pagamento_taxa;  
+    
+    
+    //metodo construtor 
+    public Estabelecimento() {
+    }
+    
+    //metodos especiais(funcionalidades)
+    public void cadastro() {
+    }
+
+    public void login() {
+    }
+
+    public void atualizarDados() {
+    }
+
+    public void atualziarDispositivo() {
+    }
+
+    public void renovarMensalidade() {
+    }
+
+    public void alterarPlanoContrado() {
+    }
+    
+    //metodos getters e setters
+    public int getId_estabelecimento() {
+        return id_estabelecimento;
+    }
+
+    public void setId_estabelecimento(int id_estabelecimento) {
+        this.id_estabelecimento = id_estabelecimento;
+    }
+
+    public String getNome_estabelecimento() {
+        return nome_estabelecimento;
+    }
+
+    public void setNome_estabelecimento(String nome_estabelecimento) {
+        this.nome_estabelecimento = nome_estabelecimento;
+    }
+
+    public int getCpnj() {
+        return cpnj;
+    }
+
+    public void setCpnj(int cpnj) {
+        this.cpnj = cpnj;
+    }
+
+    public int getCep_estabelecimento() {
+        return cep_estabelecimento;
+    }
+
+    public void setCep_estabelecimento(int cep_estabelecimento) {
+        this.cep_estabelecimento = cep_estabelecimento;
+    }
+
+    public String getLogadouro_estabelecimento() {
+        return logadouro_estabelecimento;
+    }
+
+    public void setLogadouro_estabelecimento(String logadouro_estabelecimento) {
+        this.logadouro_estabelecimento = logadouro_estabelecimento;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAcessi_auditiva() {
+        return acessi_auditiva;
+    }
+
+    public void setAcessi_auditiva(String acessi_auditiva) {
+        this.acessi_auditiva = acessi_auditiva;
+    }
+
+    public String getAcessi_fisico() {
+        return acessi_fisico;
+    }
+
+    public void setAcessi_fisico(String acessi_fisico) {
+        this.acessi_fisico = acessi_fisico;
+    }
+
+    public String getAcessi_nenhuma() {
+        return acessi_nenhuma;
+    }
+
+    public void setAcessi_nenhuma(String acessi_nenhuma) {
+        this.acessi_nenhuma = acessi_nenhuma;
+    }
+
+    public String getHorario_funcionamento() {
+        return horario_funcionamento;
+    }
+
+    public void setHorario_funcionamento(String horario_funcionamento) {
+        this.horario_funcionamento = horario_funcionamento;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public float getTaxa_cancelamento() {
+        return taxa_cancelamento;
+    }
+
+    public void setTaxa_cancelamento(float taxa_cancelamento) {
+        this.taxa_cancelamento = taxa_cancelamento;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConcorda() {
+        return concorda;
+    }
+
+    public void setConcorda(String concorda) {
+        this.concorda = concorda;
+    }
+
+    public String getConcorda_newstalleter() {
+        return concorda_newstalleter;
+    }
+
+    public void setConcorda_newstalleter(String concorda_newstalleter) {
+        this.concorda_newstalleter = concorda_newstalleter;
+    }
+
+    public boolean isDisponibilidade() {
+        return disponibilidade;
+    }
+
+    public void setDisponibilidade(boolean disponibilidade) {
+        this.disponibilidade = disponibilidade;
+    }
+
+    public int getId_planos() {
+        return id_planos;
+    }
+
+    public void setId_planos(int id_planos) {
+        this.id_planos = id_planos;
+    }
+
+    public int getId_pagamento_taxa() {
+        return id_pagamento_taxa;
+    }
+
+    public void setId_pagamento_taxa(int id_pagamento_taxa) {
+        this.id_pagamento_taxa = id_pagamento_taxa;
+    }
     
 }
--- PI3Konex_Rapina/src/main/java/Classes/Reserva.java
+++ PI3Konex_Rapina/src/main/java/Classes/Reserva.java
@@ -1,14 +1,110 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Classes;
 
 /**
  *
- * @author zica_
+ * @author Gabriel Felipe
  */
 public class Reserva {
-    
+
+    //atributos da reserva
+    public int num_reserva;
+    public int id_cliente;
+    public int id_estabelecimento;
+    public int qtd_acompanhantes;
+    public String data_criada;
+    public String data_reservada;
+    public String horario_reservado;
+    private String hora_valida_reserva;
+    private String hora_valida_saida;
+
+    //metodo construtor
+    public Reserva() {
+    }
+
+    //metodos especiais(funcionalidades)
+    public void consultarReservasUser() {
+    }
+
+    public void consultarReservasAgendas() {
+    }
+
+    public void cancelarReserva() {
+    }
+
+    public void alterarReserva() {
+    }
+
+    //metodos getters e setters
+    public int getNum_reserva() {
+        return num_reserva;
+    }
+
+    public void setNum_reserva(int num_reserva) {
+        this.num_reserva = num_reserva;
+    }
+
+    public int getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(int id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public int getId_estabelecimento() {
+        return id_estabelecimento;
+    }
+
+    public void setId_estabelecimento(int id_estabelecimento) {
+        this.id_estabelecimento = id_estabelecimento;
+    }
+
+    public int getQtd_acompanhantes() {
+        return qtd_acompanhantes;
+    }
+
+    public void setQtd_acompanhantes(int qtd_acompanhantes) {
+        this.qtd_acompanhantes = qtd_acompanhantes;
+    }
+
+    public String getData_criada() {
+        return data_criada;
+    }
+
+    public void setData_criada(String data_criada) {
+        this.data_criada = data_criada;
+    }
+
+    public String getData_reservada() {
+        return data_reservada;
+    }
+
+    public void setData_reservada(String data_reservada) {
+        this.data_reservada = data_reservada;
+    }
+
+    public String getHorario_reservado() {
+        return horario_reservado;
+    }
+
+    public void setHorario_reservado(String horario_reservado) {
+        this.horario_reservado = horario_reservado;
+    }
+
+    public String getHora_valida_reserva() {
+        return hora_valida_reserva;
+    }
+
+    public void setHora_valida_reserva(String hora_valida_reserva) {
+        this.hora_valida_reserva = hora_valida_reserva;
+    }
+
+    public String getHora_valida_saida() {
+        return hora_valida_saida;
+    }
+
+    public void setHora_valida_saida(String hora_valida_saida) {
+        this.hora_valida_saida = hora_valida_saida;
+    }
+
 }
