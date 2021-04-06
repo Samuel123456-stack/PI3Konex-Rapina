8e7ca1192f4ffd449eb9b2984faa887a1b5efddd
Rubens_Ricci <rubenslricci@outlook.com.br>
2021-04-05 22:39:00 -0300

    parte

--- Arquivos importantes/test_db.sql
+++ Arquivos importantes/test_db.sql
@@ -18,3 +18,13 @@ insert into tbreserva (nomeCli,dataRes,statusRes) values('Arnaldo Soares','2021-
 select * from tbreserva;
 select *, DATE_FORMAT(dataRes, '%d/%m/%Y  %H:%i') from tbreserva; 
 
+create table tbmeusPag(
+idPag int auto_increment primary key,
+NomeTaxa varchar(45) not null,
+dataTaxa date,
+valor decimal(6,2)
+);
+
+insert into tbmeusPag (NomeTaxa, dataTaxa,valor) values('mensalidade', '2021-03-28', '49.90'),('mensalidade', '2021-03-28', '49.00');
+
+select * from tbmeusPag;
